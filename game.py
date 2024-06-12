83e138c75c7027f05fd2083886cb3b7838eb4beb
Liam S. <liam.alan.smth@gmail.com>
2024-06-12 13:31:19 +0100

    Implement check for diagonal wins.

---
+++ game.py
@@ -0,0 +1,36 @@
+import user_interface
+from player import Player
+from grid import Grid
+import random
+
+user_interface.show_welcome()
+user_interface.show_instructions()
+
+player1_marker, player2_marker = user_interface.get_player_markers()
+players = [Player(number=1, marker=player1_marker), Player(number=2, marker=player2_marker)]
+
+grid_rows, grid_columns = user_interface.get_grid_size()
+grid = Grid(grid_rows, grid_columns)
+
+user_interface.show_begin()
+
+markers_placed = 0
+game_over = False
+current_player = random.choice(players)
+while not game_over:
+    column_choice, row_choice = user_interface.ask_player_place_marker(current_player)
+    grid.place_marker(current_player, column_choice, row_choice)
+    markers_placed += 1
+    user_interface.show_grid(grid)
+
+
+    if markers_placed == grid.total_cells:
+        user_interface.show_game_over(players[0], players[1])
+        game_over = not user_interface.ask_play_again()
+    else:
+        winner = grid.check_if_won(current_player, column_choice, row_choice)
+        if winner:
+            print("Winner!")
+            current_player.score += 1
+
+    current_player = players[0] if current_player == players[1] else players[1]
--- grid.py
+++ grid.py
@@ -1,10 +1,39 @@
+from player import Player
+
+
 class Grid:
     def __init__(self, height, width):
         self.height = height
+        # TODO change to size. Game only really makes sense on an nxn grid.
         self.width = width
+        self.total_cells = height * width
+        self.array = []
+        for _ in range(height):
+            self.array.append(['_']*width)
 
-    def place_marker(self, player, x, y):
-        pass
+    def place_marker(self, player_, across, down):
+        self.array[down][across] = player_.marker
 
-    def get_marker(self, x, y):
-        return "X"
+    def get_marker(self, across, down):
+        return self.array[down][across]
+
+    def check_diagonal_win(self, column_choice: int, row_choice: int) -> bool:
+        placed_marker = self.array[column_choice][row_choice]
+        if column_choice != row_choice:
+            return False
+        else:
+            for i in range(self.width):
+                if not self.array[i][i] == placed_marker:
+                    break
+                elif i == self.width-1:
+                    return True
+
+            for i in range(self.width):
+                if not self.array[i][-i] == placed_marker:
+                    break
+                elif i == self.width-1:
+                    return True
+            return False
+
+    def check_if_won(self, player: Player, column_choice: int, row_choice: int) -> bool:
+        return self.check_diagonal_win(column_choice, row_choice)
